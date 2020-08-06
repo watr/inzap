3db6da68a7412dba11d608316b0e4eee6130f3a6
watr <watr@users.noreply.github.com>
2020-08-06 18:53:56 +0900

    add project files

---
+++ IN-ZAP/IN-ZAP/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IN-ZAP
+//
+//  Created by HASHIMOTO Wataru on 2020/08/06.
+//  Copyright © 2020 Prancee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IN-ZAP/IN-ZAP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IN-ZAP
+//
+//  Created by HASHIMOTO Wataru on 2020/08/06.
+//  Copyright © 2020 Prancee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
